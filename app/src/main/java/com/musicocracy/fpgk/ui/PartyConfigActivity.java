8887bacd62262ef6fe4464b56419343628c896f7
Jon Povirk <jmp180@pitt.edu>
2016-11-18 15:57:09 -0500

    party config

--- app/src/main/java/com/musicocracy/fpgk/ui/PartyConfigActivity.java
+++ app/src/main/java/com/musicocracy/fpgk/ui/PartyConfigActivity.java
@@ -2,12 +2,65 @@ package com.musicocracy.fpgk.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class PartyConfigActivity extends AppCompatActivity {
+    private EditText tokenCountPicker;
+    private EditText tokenMinutePicker;
+    private EditText tokenSecondPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party_config);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_open_filter:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // http://stackoverflow.com/questions/14212518/is-there-a-way-to-define-a-min-and-max-value-for-edittext-in-android
+    private class InputFilterMinMax implements InputFilter {
+        private int min, max;
+
+        public InputFilterMinMax(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        public InputFilterMinMax(String min, String max) {
+            this.min = Integer.parseInt(min);
+            this.max = Integer.parseInt(max);
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            try {
+                // Remove the string out of destination that is to be replaced
+                String newVal = dest.toString().substring(0, dstart) + dest.toString().substring(dend, dest.toString().length());
+                // Add the new string in
+                newVal = newVal.substring(0, dstart) + source.toString() + newVal.substring(dstart, newVal.length());
+                int input = Integer.parseInt(newVal);
+                if (isInRange(min, max, input))
+                    return null;
+            } catch (NumberFormatException nfe) { }
+            return "";
+        }
+
+        private boolean isInRange(int a, int b, int c) {
+            return b > a ? c >= a && c <= b : c >= b && c <= a;
+        }
     }
 }
