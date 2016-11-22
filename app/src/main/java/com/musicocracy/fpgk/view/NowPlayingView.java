b99fa8d171a9c50b0a0419650e292a0490ad8ba0
Trevor Fry <tef14@pitt.edu>
2016-11-22 16:34:19 -0500

    Interfaces for NowPlaying screen and SongSelect Screen.

--- app/src/main/java/com/musicocracy/fpgk/view/NowPlayingView.java
+++ app/src/main/java/com/musicocracy/fpgk/view/NowPlayingView.java
@@ -1,4 +1,9 @@
 package com.musicocracy.fpgk.view;
 
+import java.util.List;
+
 public interface NowPlayingView {
+    void updateArtist(String artistName);
+    void updateSong(String songName);
+    void updateVotableSongs(List<String> votableSongs);
 }
--- app/src/main/java/com/musicocracy/fpgk/view/SongSelectView.java
+++ app/src/main/java/com/musicocracy/fpgk/view/SongSelectView.java
@@ -1,4 +1,7 @@
 package com.musicocracy.fpgk.view;
 
+import java.util.List;
+
 public interface SongSelectView {
+    void updateSongs(List<String> songs);
 }
