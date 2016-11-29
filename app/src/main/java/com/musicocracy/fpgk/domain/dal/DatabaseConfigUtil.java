3ab0fe0e605505b5c89e27877404565d7308ee3b
Jon Povirk <jmp180@pitt.edu>
2016-11-29 02:50:15 -0500

    Database config file update. Added ListView style

--- app/src/main/java/com/musicocracy/fpgk/domain/dal/DatabaseConfigUtil.java
+++ app/src/main/java/com/musicocracy/fpgk/domain/dal/DatabaseConfigUtil.java
@@ -15,11 +15,13 @@ import java.io.File;
 
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
     public static void main(String[] args) throws Exception {
+        System.out.println("test");
         String currDirectory = "user.dir";
         String configPath = "/app/src/main/res/raw/ormlite_config.txt";
         String projectRoot = System.getProperty(currDirectory);
 
         String fullConfigPath = projectRoot + configPath;
+        System.out.println("Writing OrmLiteConfig to " + fullConfigPath);
 
         File configFile = new File(fullConfigPath);
 
